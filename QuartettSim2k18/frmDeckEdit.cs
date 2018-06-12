2e57cace5562a78416c0a1a09a8859fb8bb7e199
NiclasKrosse <39406700+NiclasKrosse@users.noreply.github.com>
2018-06-12 19:09:41 +0200

    Cleanup

--- QuartettSim2k18/frmDeckEdit.cs
+++ QuartettSim2k18/frmDeckEdit.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using DevExpress.XtraEditors;
 
 namespace QuartettSim2k18
 {
